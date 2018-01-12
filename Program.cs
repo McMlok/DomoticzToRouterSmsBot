e4b472de03643a79f7b215ae6e653cf6bfd756b5
McMlok <tomasovicm@seznam.cz>
2018-01-12 22:31:13 +0100

    create console proj from template

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DomoticzToRouterSmsBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
